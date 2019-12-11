4563473782471b435e3d3af47a0edcbf9e28c8f6
Andrew Sugaya <andrew.sugaya@upskill.io>
2019-12-11 14:58:32 -0500

    Removed submodules

---
+++ Examples/CreateUserManagerGroup/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.IO;
+using System;
+using Microsoft.Extensions.Configuration;
+using Skylight.Client;
+using System.Threading.Tasks;
+using Skylight.Api.Assignments.V1.Models;
+
+namespace CreateUserManagerGroup
+{
+    class Program
+    {
+        public static ApiClient ApiClient;
+        static async Task Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(path, ".."))
+                .AddJsonFile("exampleSettings.json")
+                .Build();
+            
+            var username = config["username"];
+            var password = config["password"];
+            var realm = config["domain"];
+            var apiUri = config["apiUrl"];
+            var mqttUrl = config["mqttUrl"];
+            
+            var connection = new ConnectionInfo(username, password, realm, apiUri);
+            
+            ApiClient = new ApiClient(connection);
+
+            var getRequest = new Skylight.Api.Media.V3.GetListFileInfosRequest();
+            var result = await ApiClient.ExecuteRequestAsync(getRequest);
+            foreach(Skylight.Api.Media.V3.Models.FileInfo info in result.Content){
+                Console.WriteLine(info.Filename);
+            }
+            Console.WriteLine("End of example");
+            
+        }
+    }
+}
