4fe0e4c57b7f9fe53ebffa4fc7ed715193f9b68e
Andrew Sugaya <andrew.sugaya@upskill.io>
2019-12-16 14:25:58 -0500

    Adding Skytool info files

---
+++ Examples/Template/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Template
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new ConnectionInfo(username, password, realm, apiUri);
+            
+            ApiClient = new ApiClient(connection);
+
+            var getRequest = new Skylight.Api.Media.V3.GetListFileInfosRequest();
+            var result = await ApiClient.ExecuteRequestAsync(getRequest);
+            foreach(Skylight.Api.Media.V3.Models.FileInfo info in result.Content){
+                Console.WriteLine(info.Filename);
+            }
+            Console.WriteLine("End of example");
+        }
+    }
+}
