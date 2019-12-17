f6db23af33387a2ba56ced4c5ee1a9878b6fb3a5
Andrew Sugaya <andrew.sugaya@upskill.io>
2019-12-17 14:37:01 -0500

    Added more error handling

--- Skylight.Sdk/Program.cs
+++ Skylight.Sdk/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.ObjectModel;
+using System;
 using System.IO;
 using Newtonsoft.Json;
 using Skylight.Client;
@@ -8,19 +9,31 @@ namespace Skylight.Sdk
     public class Manager
     {
         public ApiClient ApiClient;
-        public Manager(string credentialsPath = "credentials.json") {
-            
-            //Read in our credentials
-            StreamReader reader = new StreamReader(credentialsPath);
-            string json = reader.ReadToEnd();
-            reader.Close();
-            dynamic credentials = JsonConvert.DeserializeObject(json);
+
+        private dynamic Credentials;
+        public Manager(string credentialsPath ) {
+            if(!this.ReadCredentials(credentialsPath)) return;
             
             //Set up a new connection
-            var connection = new ConnectionInfo((string)credentials.username, (string)credentials.password, (string)credentials.domain, (string)credentials.apiUrl);
+            var connection = new ConnectionInfo((string)Credentials.username, (string)Credentials.password, (string)Credentials.domain, (string)Credentials.apiUrl);
 
             //Use the connection to create a client
             ApiClient = new ApiClient(connection);
+        }
+
+        private bool ReadCredentials(string credentialsPath = "credentials.json") {
+            try {
+                //Read in our credentials
+                StreamReader reader = new StreamReader(credentialsPath);
+                string json = reader.ReadToEnd();
+                reader.Close();
+                Credentials = JsonConvert.DeserializeObject(json);
+                return true;
+            } catch {
+                //Either the file doesn't exist, or the file's contents are corrupted
+                Console.Error.WriteLine("Please ensure credentials.json path points to a file with valid Skylight API credentials. If using the Skytool CLI, copy the API credentials to the credentials.json file in the root working directory.");
+                return false;
+            }
         }
     }
 }
