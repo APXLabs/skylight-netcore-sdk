b2c469f7213c368e6de39389b301c50ffd56fc1f
Andrew Sugaya <andrew.sugaya@upskill.io>
2019-12-17 09:27:02 -0500

    Modified template example

--- Examples/Template/Program.cs
+++ Examples/Template/Program.cs
@@ -1,21 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace Template
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var connection = new ConnectionInfo(username, password, realm, apiUri);
-            
-            ApiClient = new ApiClient(connection);
+        var connection = new ConnectionInfo(username, password, realm, apiUri);
+        
+        ApiClient = new ApiClient(connection);
 
-            var getRequest = new Skylight.Api.Media.V3.GetListFileInfosRequest();
-            var result = await ApiClient.ExecuteRequestAsync(getRequest);
-            foreach(Skylight.Api.Media.V3.Models.FileInfo info in result.Content){
-                Console.WriteLine(info.Filename);
-            }
-            Console.WriteLine("End of example");
+        var getRequest = new Skylight.Api.Media.V3.GetListFileInfosRequest();
+        var result = await ApiClient.ExecuteRequestAsync(getRequest);
+        foreach(Skylight.Api.Media.V3.Models.FileInfo info in result.Content){
+            Console.WriteLine(info.Filename);
         }
+        Console.WriteLine("End of example");
     }
 }
+
