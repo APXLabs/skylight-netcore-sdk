fdd17d6bbe3af5f4d751eceb6034b985f9b6104a
Andrew Sugaya <andrew.sugaya@upskill.io>
2019-12-17 13:21:21 -0500

    Updated template program.cs

--- Examples/Template/Program.cs
+++ Examples/Template/Program.cs
@@ -1,28 +1,22 @@
-﻿using System;
+﻿
 using System.IO;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System;
+using Microsoft.Extensions.Configuration;
 using Skylight.Client;
+using System.Threading.Tasks;
+using Skylight.Sdk;
 
 class Program
 {
+    public static Manager Manager;
     static async Task Main(string[] args)
     {
-        //Read in our credentials
-        StreamReader reader = new StreamReader("credentials.json");
-        string json = reader.ReadToEnd();
-        reader.Close();
-        dynamic credentials = JsonConvert.DeserializeObject(json);
+        //Create our manager and point it to our credentials file
+        Manager = new Manager(Path.Combine("..", "..", "credentials.json"));
         
-        //Set up a new connection
-        var connection = new ConnectionInfo((string)credentials.username, (string)credentials.password, (string)credentials.domain, (string)credentials.apiUrl);
-
-        //Use the connection to create a client
-        var ApiClient = new ApiClient(connection);
-
         //This is a sample GET request to list all the files in the domain
         var getRequest = new Skylight.Api.Media.V3.GetListFileInfosRequest();
-        var result = await ApiClient.ExecuteRequestAsync(getRequest);
+        var result = await Manager.ApiClient.ExecuteRequestAsync(getRequest);
         foreach(Skylight.Api.Media.V3.Models.FileInfo info in result.Content){
             Console.WriteLine(info.Filename);
         }
