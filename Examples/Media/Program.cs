aa374ff6e129a4b5c7774308c2c631cc0d825263
Andrew Sugaya <andrew.sugaya@upskill.io>
2020-01-13 10:29:05 -0500

    Adding several examples

---
+++ Examples/Media/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.IO;
+using System;
+using Skylight.Client;
+using System.Threading.Tasks;
+using Skylight.Sdk;
+
+namespace Media
+{
+    class Program
+    {
+        public static Manager Manager;
+        static async Task Main(string[] args)
+        {
+            try {
+                //@skydocs.start(authentication.login)
+                /*
+                    Create our manager by passing it the path to our credentials.json file.
+                    This json file has our API credentials copy-pasted from Skylight Web.
+                    The path is also optional; the constructor for the SDK's manager can also take 0 arguments, in which case it will search for a file called `credentials.json` in the root directory of the extension.
+                */
+                Manager = new Manager(Path.Combine("..", "..", "credentials.json"));
+                //@skydocs.end()
+            } catch { return; }
+
+            await Manager.MediaClient.UploadFile(new FileInfo(Path.Join(".", "files", "test.png")), "SDK Upload Test", "This is a file uploaded using the Skylight C# SDK");
+            
+        }
+    }
+}
